f91d82e2601bbe432094325122409ad98bf1f890
Cristian-000 <129860008+Cristian-000@users.noreply.github.com>
2023-09-12 20:06:46 -0300

    2

--- logica.js
+++ logica.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("buscarButton").addEventListener("click", function () {
 
-        /* What is Waifu.im?
-        Waifu.im is an easy to use API that allows you to get waifu pictures 
-        from an archive of over 4000 images and multiple tags!*/
-
-        // Esta API no necesita autorizacion como tokens o apikey
         const selectedTag = document.getElementById("tagSelector").value;
 
         const apiUrl = 'https://api.waifu.im/search';
+
         // Parametros permitidos por la API, acepta tags en la peticion
         const params = {
             included_tags: selectedTag,
@@ -18,10 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
         // URLSearchParams es una parte de la API de URL de JavaScript
         // que se utiliza para trabajar con los parámetros de consulta de una URL.
         // resultado de params ?included_tags=maid&height=%3E=2000
-        const queryParams = new URLSearchParams(params);
-        const requestUrl = `${apiUrl}?${queryParams}`;
 
-        fetch(requestUrl)
+        const queryParams = new URLSearchParams(params);
+        
+        const urlFinal = `${apiUrl}?${queryParams}`;
+
+        fetch(urlFinal)
             .then(response => {
                 if (response.ok) {
                     return response.json();
@@ -30,21 +28,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             })
             .then(data => {
-
                 console.log(data);
 
-                const imageUrl = data.images[0].url; // cargamos la primera imagen
+                const imageUrl = data.images[0].url;
 
                 const contenedorImagenes = document.getElementById('contenedor-imagenes');
 
-                // Crear una nueva imagen
                 const imagen = new Image();
                 imagen.src = imageUrl;
 
-                // Eliminamos la imagen anterior si la hubiera y mostramos la nueva
                 contenedorImagenes.innerHTML = ""
                 contenedorImagenes.appendChild(imagen);
-
             })
             .catch(error => {
                 console.error('Se produjo un error:', error.message);
